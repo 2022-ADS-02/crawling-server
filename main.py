e0e60e6174f6139bb85a20a6376bbf63615c11c9
ggyul <wakegyul@gmail.com>
2022-12-18 18:28:48 +0900

    feat: JWT 헤더에서 id와 토큰 읽는다

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import jwt
 import pymysql
 import yaml
 from os import environ
@@ -7,22 +8,28 @@ from flask import Flask, request
 import py_eureka_client.eureka_client as eureka_client
 import time
 import nest_asyncio
+
 nest_asyncio.apply()
 
 boj_url = 'https://www.acmicpc.net/'
 language_numbers = {'Python': '28', 'Java': '93'}  # jdk 11
 
-with open("./db/access.yaml", encoding="UTF-8") as f:
-    cfg = yaml.load(f, Loader=yaml.FullLoader)
-    db_host = cfg["db_host"]
-    db_user = cfg["db_user"]
-    db_password = cfg["db_password"]
-    db_name = cfg["db_name"]
+# with open("./db/access.yaml", encoding="UTF-8") as f:
+#     cfg = yaml.load(f, Loader=yaml.FullLoader)
+#     db_host = cfg["db_host"]
+#     db_user = cfg["db_user"]
+#     db_password = cfg["db_password"]
+#     db_name = cfg["db_name"]
 
-conn = pymysql.connect(host=db_host, port=3306,
-                       user=db_user, password=db_password, db=db_name,
-                       charset="utf8", cursorclass=pymysql.cursors.DictCursor)
-curs = conn.cursor()
+with open("./auth/token.yaml", encoding="UTF-8") as f:
+    cfg = yaml.load(f, Loader=yaml.FullLoader)
+    access_key = cfg["jwt"]["secret"]["access"]
+
+
+# conn = pymysql.connect(host=db_host, port=3306,
+#                        user=db_user, password=db_password, db=db_name,
+#                        charset="utf8", cursorclass=pymysql.cursors.DictCursor)
+# curs = conn.cursor()
 
 eureka_client.init(eureka_server="{}:8761/eureka" .format(environ.get("EUREKA_ADDRESS", "192.168.2.11")),
                    app_name="crawling-service",
@@ -39,6 +46,7 @@ def get_problem_info(number: str):
         return search_db(number)
 
     return crawl(number)
+
 
 def crawl(prlblem_id):
     url = boj_url + 'problem/' + prlblem_id
@@ -65,18 +73,24 @@ def crawl(prlblem_id):
     save_result(prlblem_id, crawling_result)
     return crawling_result
 
+
 '''
 {
-    "id": "jamjoa",
-    "boj_autologin": "3c176...",
+    # "id": "jamjoa",
+    # "boj_autologin": "3c176...",
     "language":"Python",
     "source":"a,b = map(int, input().split())\nprint(a+b)"
 }
 '''
 @app.route("/search/submit/<number>", methods=['POST'])
 def submitCodeToBoj(number: str):
-    if 'id' not in request.headers or 'bojToken' not in request.headers:
+    if 'Authorization' not in request.headers:
         return "토큰을 확인해주세요", 401
+    token = request.headers['Authorization'].replace('Bearer', '').strip()
+    decoded_token = jwt.decode(token, access_key, algorithms="HS256")
+    member_id = decoded_token['id']
+    boj_token = decoded_token['bojToken']
+    print(token, decoded_token, member_id, boj_token)
 
     req = request.get_json()
 
@@ -85,10 +99,8 @@ def submitCodeToBoj(number: str):
     }
     response = requests.get('https://www.acmicpc.net', headers=headers_dict)
 
-
-
     cookies_dict = {
-        'bojautologin': request.headers['bojToken'],
+        'bojautologin': boj_token,
         'OnlineJudge': response.cookies.get_dict()['OnlineJudge']
     }
 
@@ -118,18 +130,20 @@ def submitCodeToBoj(number: str):
     response = requests.post(url, headers=headers_dict, data=payload, cookies=cookies_dict)
     if response.status_code != 200:
         return {"result": "제출에 실패했습니다."}
-
-    url = boj_url + 'status?user_id=' + request.headers['id']
+    url = boj_url + 'status?user_id=' + member_id
     response = requests.get(url)
     response.raise_for_status()  # OK 아닌 경우 오류
     soup = BeautifulSoup(response.text, "lxml")
     rows = soup.select('tbody tr')
-    submitted_number = -1
+    submitted_number = ""
     for row in rows:
-        if row.find('a', attrs={"class": "problem_title"}).text == number:
+        problem_title = row.find('a', attrs={"class": "problem_title"})
+        if problem_title and problem_title.text == number:
             submitted_number = row.find('td').text
             break
     print(submitted_number)
+    if submitted_number == "":
+        return "백준 아이디와 동일한 아이디가 아닙니다.", 401
 
     success = False
     judge_result = ""
@@ -182,7 +196,7 @@ def save_result(problem_id, result):
 
 
 def exists(problem_id):
-    sql = "select exists(select * from problems where problem_id = {}) as result" .format(problem_id)
+    sql = "select exists(select * from problems where problem_id = {}) as result".format(problem_id)
     curs.execute(sql)
     result = curs.fetchone()
     return result["result"]
@@ -190,7 +204,7 @@ def exists(problem_id):
 
 def search_db(problem_id: str):
     crawling_result = {"samples": [], "samples_text": []}
-    sql = "select * from problems where problem_id = {}" .format(problem_id)
+    sql = "select * from problems where problem_id = {}".format(problem_id)
     curs.execute(sql)
     problem = curs.fetchone()
     if problem:
@@ -201,7 +215,7 @@ def search_db(problem_id: str):
     sql = "select s.input as s_input, s.output as s_output, st.input as st_input, st.output as st_output " \
           "from samples s, samples_text st " \
           "where s.problem_id = st.problem_id " \
-          "and s.problem_id = {}" .format(problem_id)
+          "and s.problem_id = {}".format(problem_id)
     curs.execute(sql)
     samples = curs.fetchone()
     while samples:
